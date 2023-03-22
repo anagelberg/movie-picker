c9bfaf82a16dbdcad183c62da901e177cb4cf117
anagelberg <62032317+anagelberg@users.noreply.github.com>
2023-03-22 15:50:20 -0700

    Add manager objects to clean code

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, redirect, url_for, request
 import requests
-import os
 from app import db, Movie, MovieList, app
-from manager import DbManager
+from manager import DbManager, SearchManager
+from forms import AddMovieForm, RateMovieForm, NewWatchlistForm
 
 # To do- Wednesday
 # TODO: Edit page for movie/manual entry button
-# TODO: Make an object to handle some search functionality Manager /clean up code
+# TODO: NavBar
 # TODO: Can select page go under the search box?
 
 # To Do - Thursday
 # TODO: Add movie picker functionality
+# TODO: Add "watched" feature
 
 # To do- Friday
+# TODO: Make PRETTY
+# TODO: Requirements
 # TODO: Make comments on code
 # TODO: Make instructional README
 # TODO: Download on Jesse's computer
@@ -22,14 +25,11 @@ from manager import DbManager
 # TODO: Same movie different list?
 # TODO: trycatch errors: double click
 
-##########################################
-TMDB_API_KEY = os.environ['TMDB_API_KEY']
-##########################################
 
 db.create_all()
-from forms import AddMovieForm, RateMovieForm, NewWatchlistForm
 
 db_manager = DbManager()
+search_manager = SearchManager()
 
 @app.route("/")
 def home():
@@ -68,60 +68,39 @@ def add(watchlist_id):
     add_form = AddMovieForm()
     db_manager.current_watchlist = MovieList.query.filter_by(id=watchlist_id).first()
     if add_form.validate_on_submit():
-        movie_title = add_form.movie.data
-        parameters = {
-            'api_key': TMDB_API_KEY,
-            'language': 'en-US',
-            'query': movie_title,
-        }
-        response = requests.get(url='https://api.themoviedb.org/3/search/movie', params=parameters)
-        response.raise_for_status()
-        data = response.json()
-        movie_data = data['results']
-
-        all_movies = Movie.query.all()
-        id_list = [int(movie.tmdb_id) for movie in all_movies]
-        return render_template("select.html", movies=movie_data, movie_id_list=id_list)
+        search_manager.search_tmdb_titles(title=add_form.movie.data)
+        db_manager.current_id_list = [int(movie.tmdb_id) for movie in Movie.query.all()]
+        return render_template("select.html", movies=search_manager.title_results,
+                               movie_id_list=db_manager.current_id_list)
     return render_template("add.html", form=add_form)
 
 # Adds the movie details to the database sent from select
 @app.route("/add-movie/<movie_id>")
 def add_movie(movie_id):
-    parameters = {
-        'api_key': TMDB_API_KEY,
-    }
-    response = requests.get(url=f'https://api.themoviedb.org/3/movie/{movie_id}', params=parameters)
-    response.raise_for_status()
-    movie_data = response.json()
-
-    if movie_data["poster_path"] is not None:
-        poster_url = movie_data["poster_path"]
-    else:
-        poster_url = movie_data['belongs_to_collection']['poster_path']
-
-    genre_list = [genre["name"] for genre in movie_data["genres"]]
+    search_manager.search_tmdb_details(movie_id)
 
     # Add the movie to the database
     new_movie = Movie(
-        title=movie_data["title"],
-        year=movie_data["release_date"].split("-")[0],
-        description=movie_data["overview"],
+        title=search_manager.movie_data["title"],
+        year=search_manager.movie_data["release_date"].split("-")[0],
+        description=search_manager.movie_data["overview"],
         my_rating=None,
-        img_url=f"https://image.tmdb.org/t/p/original{poster_url}",
-        run_time=movie_data["runtime"],
-        pop_rating=movie_data["vote_average"],
-        imdb_id=movie_data["imdb_id"],
+        img_url=f"https://image.tmdb.org/t/p/original{search_manager.movie_data['poster_url']}",
+        run_time=search_manager.movie_data["runtime"],
+        pop_rating=search_manager.movie_data["vote_average"],
+        imdb_id=search_manager.movie_data["imdb_id"],
         watched="False",
-        genre=', '.join(genre_list),
-        tmdb_id=movie_data['id'],
+        genre=search_manager.movie_data["genre_string"],
+        tmdb_id=search_manager.movie_data['id'],
         movie_list=db_manager.current_watchlist
      )
 
     db.session.add(new_movie)
     db.session.commit()
 
-    # TODO: Change this to go to select, first need to make appropriate search manager object?
-    return redirect(url_for('home'))
+    db_manager.current_id_list = [int(movie.tmdb_id) for movie in Movie.query.all()]
+    return render_template("select.html", movies=search_manager.title_results,
+                           movie_id_list=db_manager.current_id_list)
 
 
 
--- manager.py
+++ manager.py
@@ -1,6 +1,45 @@
+import os
+import requests
+
+
 class DbManager():
     def __init__(self):
         self.current_watchlist = None
+        self.current_id_list = None
 
 
+class SearchManager():
+    def __init__(self):
+        self.title_results = None
+        self.movie_data = None
+        self.TMDB_API_KEY = os.environ['TMDB_API_KEY']
 
+    def search_tmdb_titles(self, title):
+        parameters = {
+            'api_key': self.TMDB_API_KEY,
+            'language': 'en-US',
+            'query': title,
+        }
+        response = requests.get(url='https://api.themoviedb.org/3/search/movie', params=parameters)
+        response.raise_for_status()
+        data = response.json()
+        self.title_results = data['results']
+
+    def search_tmdb_details(self, tmdb_id):
+        parameters = {
+            'api_key': self.TMDB_API_KEY,
+        }
+        response = requests.get(url=f'https://api.themoviedb.org/3/movie/{tmdb_id}', params=parameters)
+        response.raise_for_status()
+        self.movie_data = response.json()
+
+        if self.movie_data["poster_path"]:
+            self.movie_data["poster_url"] = self.movie_data["poster_path"]
+        elif self.movie_data['belongs_to_collection']:
+            self.movie_data["poster_url"] = self.movie_data['belongs_to_collection']['poster_path']
+        else:
+            # TODO: make a placeholder?
+            self.movie_data["poster_url"] = None
+
+        genre_list = [genre["name"] for genre in self.movie_data["genres"]]
+        self.movie_data["genre_string"] = ', '.join(genre_list)
